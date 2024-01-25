a458a838664207c2a77e744a8413611b359a6051
Thad Le <llengocthangg@gmail.com>
2024-01-26 03:08:25 +0700

    Move Nav to AuthenticationProvider

--- src/app/AuthenticatedProvider.js
+++ src/app/AuthenticatedProvider.js
@@ -2,16 +2,25 @@
 
 import { useRouter, usePathname } from "next/navigation";
 import { isAuthenticated } from "@/utils/auth_helpers";
+import Nav from "@/components/Nav";
 
 const AuthenticationProvider = ({ children }) => {
   const router = useRouter();
   const pathname = usePathname();
+  const authenticated = isAuthenticated();
 
-  if (!isAuthenticated() && pathname !== '/login') {
+  if (!authenticated && pathname !== '/login') {
     router.push('/login')
   }
 
-  return children;
+  return (
+    <>
+      {authenticated && <Nav />}
+      <div className="flex justify-center items-center bg-gray-50">
+        {children}
+      </div>
+    </>
+  )
 };
 
 export default AuthenticationProvider;

9fcccc3cbfc7890ccd8722701ec3492f03fe0756
Thad Le <llengocthangg@gmail.com>
2024-02-04 16:12:51 +0700

    Refactor landing page
    Remove redirection logic and introduce button to go to urls page

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,20 @@
 'use client';
 
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-import { isAuthenticated } from "@/utils/auth_helpers";
+import Link from "next/link";
 
 const Home = () => {
-  const authenticated = isAuthenticated();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (!authenticated) {
-      router.push('/login')
-    } else {
-      router.push('/urls')
-    }
-  }, []);
-
   return (
-    <section className="flex justify-center items-center h-screen">
-      <h1 className="text-4xl uppercase">Welcome to url shorten service</h1>
-    </section>
+    <main className="flex justify-center items-center h-screen">
+      <div>
+        <h1 className="text-4xl uppercase">
+          Welcome to url shorten service
+        </h1>
+        <br />
+        <Link className="hover:text-gray-500" href="/urls">
+          Create A Url Now
+        </Link>
+      </div>
+    </main>
   );
 }
 

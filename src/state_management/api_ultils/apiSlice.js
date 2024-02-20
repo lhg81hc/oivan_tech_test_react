320f9f8bed6935afb84f1dc484b64b3a0a948d13
Thad Le <llengocthangg@gmail.com>
2024-02-20 12:39:42 +0700

    Move api/slices to under /state_management

--- src/state_management/api_ultils/apiSlice.js
+++ src/state_management/api_ultils/apiSlice.js
@@ -20,11 +20,9 @@ const baseQueryWithReAuth = async (args, api, extraOptions) => {
   let result = await baseQuery(args, api, extraOptions)
 
   if (result?.error?.originalStatus === 401) {
-    console.log('sending refresh token')
     // Send refresh token to get new access token
 
     const refreshResult = await baseQuery('/refresh', api, extraOptions);
-    console.log(refreshResult);
 
     if (refreshResult?.data) {
       const user = api.getState().auth.user;

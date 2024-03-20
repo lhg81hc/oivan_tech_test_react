6f3ab1c20f5578b245d5919d41ade16c792f0483
Thad Le <llengocthangg@gmail.com>
2024-03-20 20:41:43 +0700

    Refactor layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,13 +7,21 @@ import { shareConstants } from "@/constants";
 
 import AuthenticatedLayout from "@/components/AuthenticatedLayout";
 import PublicLayout from "@/components/PublicLayout";
+import { StoreProvider } from "@/state-management";
+import { ModalRoot } from "@/components/modal";
+import Link from "next/link";
 
 import "./globals.css";
-import {StoreProvider} from "@/state-management";
-import {ModalRoot} from "@/components/modal";
 
 export default function RootLayout({ children }) {
-  // const isAuth = isAuthenticated();
+  const isAuth = isAuthenticated();
+
+  const handleLogoutClick = (e) => {
+    e.preventDefault();
+    // dispatch(logOut());
+    // router.push('/login');
+  }
+
   // const pathname = usePathname();
 
   // if (!isAuth && !shareConstants.PUBLIC_ROUTES.includes(pathname)) {
@@ -28,19 +36,42 @@ export default function RootLayout({ children }) {
   // }
 
   // const ConditionalLayout = isAuth ? AuthenticatedLayout : PublicLayout
-  // const ConditionalLayout = PublicLayout
+  // // const ConditionalLayout = PublicLayout
   //
   // return <ConditionalLayout>{children}</ConditionalLayout>;
 
+
   return (
-    <html lang="en">
-      <body className="min-h-screen">
+    <html>
+      <body>
         <StoreProvider>
+          {/*<header>*/}
+          {/*  <nav>*/}
+          {/*    <div className="flex lg:flex-1">*/}
+          {/*      <a href="/" className="-m-1.5 p-1.5">*/}
+          {/*        <img className="w-16" src="/logo.png" alt="..."/>*/}
+          {/*      </a>*/}
+          {/*    </div>*/}
+          {/*    <div className="flex lg:flex-1">*/}
+          {/*      <ul className="flex items-center gap-[4vw] w-full justify-end">*/}
+          {/*        <li>*/}
+          {/*          <Link className="hover:text-gray-500" href="/urls">URLs</Link>*/}
+          {/*        </li>*/}
+          {/*        <li>*/}
+          {/*          <Link className="hover:text-gray-500" href="/users">Users</Link>*/}
+          {/*        </li>*/}
+          {/*        <li>*/}
+          {/*          <a href="#" className="hover:text-gray-500" onClick={handleLogoutClick}>Logout</a>*/}
+          {/*        </li>*/}
+          {/*      </ul>*/}
+          {/*    </div>*/}
+          {/*  </nav>*/}
+          {/*</header>*/}
           <main className="flex justify-center items-center bg-gray-50">
             {children}
           </main>
           <footer>
-            This is the footer
+            FOOTER
           </footer>
           <ModalRoot />
         </StoreProvider>
